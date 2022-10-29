8aeca443ea98da81c1e1fa6209ac5cebf0300e72
segovelo <gauchosinpingo@gmail.com>
2022-10-29 22:55:13 +0100

    sentiment-analysis project included - some changes to assemblyai_handler.py

--- sentiment-analysis/assemblyai_handler.py
+++ sentiment-analysis/assemblyai_handler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import requests
 import time
+import json
 from dotenv import load_dotenv
 import sys
 import os
@@ -8,7 +9,11 @@ import os
 load_dotenv()
 
 api_key = os.getenv('ASSEMBLYAI_API_KEY')
-filename = sys.argv[1]
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = "data/output.wav"
 upload_endpoint = 'https://api.assemblyai.com/v2/upload'
 transcript_endpoint = 'https://api.assemblyai.com/v2/transcript'
 
@@ -36,9 +41,10 @@ def upload(filename):
     return upload_response.json()['upload_url']
 
 
-def transcribe(audio_url):
+def transcribe(audio_url, sentiment_analysis):
     transcript_request = {
-        'audio_url': audio_url
+        'audio_url': audio_url,
+        'sentiment_analysis': sentiment_analysis
     }
 
     transcript_response = requests.post(
@@ -52,8 +58,8 @@ def poll(transcript_id):
     return polling_response.json()
 
 
-def get_transcription_result_url(url):
-    transcribe_id = transcribe(url)
+def get_transcription_result_url(url, sentiment_analysis=False):
+    transcribe_id = transcribe(url, sentiment_analysis)
     while True:
         data = poll(transcribe_id)
         if data['status'] == 'completed':
@@ -65,31 +71,36 @@ def get_transcription_result_url(url):
         time.sleep(30)
 
 
-def save_transcript(url, filename):
-    data, error = get_transcription_result_url(url)
+def save_transcript(url, filename, sentiment_analysis=False):
+    data, error = get_transcription_result_url(url, sentiment_analysis)
 
     if data:
-        text_filename = check_file(filename)
+        text_filename = check_file(filename, "txt")
         with open(text_filename, 'w') as f:
             f.write(data['text'])
+        if sentiment_analysis:
+            sentiment_file = check_file(filename, "json")
+            with open(sentiment_file, 'w') as f:
+                sentiments = data['sentiment_analysis_results']
+                json.dump(sentiments, f, indent=4)
         print('Transcript saved...  😊')
+        return True
     elif error:
         print("Error!!! 🙁", error)
+        return False
 
 
-def check_file(file_name):
+def check_file(file_name, ext):
     file_name = file_name.translate(
-        {ord(i): None for i in '!#@{}[]<>=+£$%^&*()?|,;:/\\\'\"'})
+        {ord(i): None for i in '!#@{}[]<>=+£$%^&*()?|,;:\\\'\"'})
     index = file_name.find(".")
     if index > 0:
         file_name = file_name[0:index]
     elif index == 0:
         file_name = file_name[1:]
-    file_name += ".txt"
-    return file_name
+    return ".".join([file_name, ext])
 
 
-#filename = "output.wav"
-audio_url = upload(filename)
+#audio_url = upload(filename)
 
-save_transcript(audio_url, filename)
+#save_transcript(audio_url, filename)
